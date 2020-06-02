a7fe52d7ccf4336240b6419e5f51cc8f8c458083
Flamislas <melvin.hersent@gmail.com>
2020-06-02 21:36:33 +0200

    seed: 'Item.destroy_all' added

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Item.destroy_all
 
 20.times do 
   kitten = ImageSuckr::GoogleSuckr.new
