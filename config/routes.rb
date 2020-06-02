6e2ed92a20b227796e5bf4a1574f898696b4fb35
cyrielle <lacyrielle@gmail.com>
2020-06-02 16:24:01 +0200

    first commit theme

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'home/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   devise_for :users
 
+root to: 'home#index'
 end
