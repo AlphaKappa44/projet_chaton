ced981f3aab97b3d4e2c4cd869743f0105df7d84
matthieuBA <matthieubacheley@gmail.com>
2020-06-04 17:42:51 +0200

    fusion

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
 
-<<<<<<< HEAD
-
-  get 'orders/create'
-=======
->>>>>>> development
   get root 'items#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
