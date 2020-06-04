b70af77eff8f8ac4b5b6bba37d9fa936a8f01e80
Flamislas <melvin.hersent@gmail.com>
2020-06-04 15:53:52 +0200

    migration done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_03_162315) do
+ActiveRecord::Schema.define(version: 2020_06_04_112827) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,15 @@ ActiveRecord::Schema.define(version: 2020_06_03_162315) do
     t.datetime "updated_at", null: false
     t.index ["cart_id"], name: "index_join_table_cart_items_on_cart_id"
     t.index ["item_id"], name: "index_join_table_cart_items_on_item_id"
+  end
+
+  create_table "join_table_order_items", force: :cascade do |t|
+    t.bigint "item_id"
+    t.bigint "order_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_join_table_order_items_on_item_id"
+    t.index ["order_id"], name: "index_join_table_order_items_on_order_id"
   end
 
   create_table "orders", force: :cascade do |t|
