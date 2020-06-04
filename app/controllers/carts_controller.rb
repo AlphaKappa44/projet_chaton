53979c2b964ec1d9a19d62597c2eca51f6227478
Alexandre, Kelifa <alexandre.kelifa@hotmail.com>
2020-06-04 13:21:28 +0200

    back and front of the show carts

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -12,4 +12,9 @@ class CartsController < ApplicationController
   def delete
   end
 
+  def show
+    @cart= Cart.find_by(user_id: current_user.id)
+    @join_table_cart_items= JoinTableCartItem.where(cart_id: @cart.id)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+
   get root 'items#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
