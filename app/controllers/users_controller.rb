062072a3a2146db2468c51c2aa22c3833131501b
Alice Wyseur <alicewyseur@newname.home>
2020-06-04 15:25:15 +0200

    FrontPageProfile/OnGoing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,14 @@
 class UsersController < ApplicationController
 
+  before_action :authenticate_user!
+  
+  def show
+    @user = User.find(params[:id])
+    if current_user == @user
+      render 'show'
+    else
+      redirect_to root_path
+    end
+  end
+
 end
