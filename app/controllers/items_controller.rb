a17980de0183af18d3549d11b02437e2ea52a2d9
matthieuBA <matthieubacheley@gmail.com>
2020-06-04 19:27:48 +0200

    debug items controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,9 @@ class ItemsController < ApplicationController
 
   def show
     @item = Item.find_by(id: params[:id])
+    unless current_user.nil?
     @cart = Cart.find_by(user_id: current_user.id)
+    end
   end
 
 end
